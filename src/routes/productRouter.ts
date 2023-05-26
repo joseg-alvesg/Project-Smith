bb8108620af40b456de6a67c401f2be32a436929
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-25 21:42:15 -0300

    remove from db

--- src/routes/productRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import productController from '../database/controllers/product.controller';
-
-const productRouter = Router();
-
-productRouter.post('/', productController.createProduct);
-
-export default productRouter;
