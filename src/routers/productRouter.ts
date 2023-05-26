05fb7a30ee1d4c86c230afd4c55ff6a8bb629a7e
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-26 00:21:00 -0300

    implements: `products get` tests

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -3,6 +3,7 @@ import productController from '../controllers/product.controller';
 
 const productRouter = Router();
 
+productRouter.get('/', productController.findAll);
 productRouter.post('/', productController.createProduct);
 
 export default productRouter;
