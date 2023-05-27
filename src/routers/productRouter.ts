c658973649bdd78bbd32f56706fef0ad8280e821
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-27 14:18:41 -0300

    implemenst: validation to `productCreation`

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import productCreateMiddlewares from '../middlewares/productCreate.middlewares';
 import productController from '../controllers/product.controller';
 
 const productRouter = Router();
 
 productRouter.get('/', productController.findAll);
-productRouter.post('/', productController.createProduct);
+productRouter.post(
+  '/',
+  productCreateMiddlewares.validName,
+  productCreateMiddlewares.validPrice,
+  productController.createProduct,
+);
 
 export default productRouter;
